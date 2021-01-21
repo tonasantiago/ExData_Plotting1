bce60176e1ed7d1e847bf4f7cf7d2db41d5808d0
Tona Santiago <tona.santiago@outlook.com>
2021-01-21 18:52:13 +0000

    First commit

---
+++ Analysis.R
@@ -0,0 +1,32 @@
+library(data.table)
+library(dplyr)
+
+
+# Get files and timestamp-----------------------------------------------------------
+
+URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+targetfile <- "exdata Fdata Fhousehold_power_consumption.zip"
+if (!file.exists(targetfile)){
+  download.file(URL, destfile = targetfile)
+}
+if (!file.exists("./exdata Fdata Fhousehold_power_consumption")){
+  unzip(targetfile)
+}
+rm(targetfile)
+rm(URL)
+
+project_time_stamp <- date()
+temporal<-file("project_time_stamp.txt")
+writeLines(project_time_stamp, temporal)
+close(temporal)
+rm(project_time_stamp)
+rm(temporal)
+
+
+# Data reading ------------------------------------------------------------
+
+bdd <- fread("household_power_consumption.txt")
+bdd[bdd == "?"] <- NA
+bdd$Date <- as.Date(bdd$Date)
+bdd$Time <- as.POSIXct(bdd$Time,format="%H:%M:%S")
+
