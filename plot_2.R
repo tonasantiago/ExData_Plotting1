af0d62c9cb6fce9f0916c1e2b313d011c957e29e
Tona Santiago <tona.santiago@outlook.com>
2021-01-24 07:49:31 +0000

    Plot 2

---
+++ plot_2.R
@@ -0,0 +1,43 @@
+library(data.table)
+library(dplyr)
+
+
+# Get files and timestamp-----------------------------------------------------------
+
+# URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# targetfile <- "exdata Fdata Fhousehold_power_consumption.zip"
+# if (!file.exists(targetfile)){
+#   download.file(URL, destfile = targetfile)
+# }
+# if (!file.exists("./exdata Fdata Fhousehold_power_consumption")){
+#   unzip(targetfile)
+# }
+# rm(targetfile)
+# rm(URL)
+# 
+# project_time_stamp <- date()
+# temporal<-file("project_time_stamp.txt")
+# writeLines(project_time_stamp, temporal)
+# close(temporal)
+# rm(project_time_stamp)
+# rm(temporal)
+
+
+
+# Data reading  and cleaning-------------------------------------------------------
+
+bdd <- fread("household_power_consumption.txt")[Date == "1/2/2007"| Date == "2/2/2007"]
+bdd[bdd == "?"] <- NA
+bdd$Date <- as.Date(bdd$Date, format="%d/%m/%Y")
+bdd$Time <- as.POSIXct(bdd$Time,format="%H:%M:%S")
+bdd$Global_active_power <- as.numeric(bdd$Global_active_power)
+
+# #Plot 2 ----------------------------------------------------------------
+
+png(file = "plot2.png", width = 480, height = 480)
+with(DT,plot(posix,
+            Global_active_power,
+            type = "l",
+            xlab = "",
+            ylab = "Global Active Power (kilowatts)"))
+dev.off()
